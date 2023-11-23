4c197e8e1f45170ab4d4bcc92ae3529f1b81ac56
aCCB <rahul.m@isbmb.ac.in>
2023-11-23 10:56:35 +0530

    added simulation with number of intermediate contexts

--- runAllWithNumIntermediateContexts.py
+++ runAllWithNumIntermediateContexts.py
@@ -74,34 +74,33 @@ if __name__ == "__main__":
     models = ['roundrobin_roundrobin', 'roundrobin_ucb', 'roundrobin_ts',
               'ucb_over_intervention_pairs', 'ts_over_intervention_pairs', 'convex_explorer']
 
-    models = ['roundrobin_ucb', 'roundrobin_ts', 'ucb_over_intervention_pairs', 'ts_over_intervention_pairs']
+    # models = ['ucb_over_intervention_pairs', 'ts_over_intervention_pairs']
 
-    exploration_budgets = [250, 500, 1000, 2500, 5000, 7500, 10000, 12500, 15000, 20000, 25000, 50000, 100000]
-
-    exploration_budgets = [10_000_000, 5_000_000, 1_000_000, 500_000, 100_000]
-    exploration_budgets = [100_000]
     # Set up the variables required to run the simulation
-    num_intermediate_contexts = 10
-    num_causal_variables = 10
-    num_interventions = num_causal_variables * 2 + 1
+    num_intermediate_contexts_list = [2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 25, 50, 100]
+    # num_intermediate_contexts_list = [100, 2, 5, 10, 20]
+    exploration_budget = 10_000
     diff_prob_transition = 0.1
     default_reward = 0.5
     diff_in_best_reward = 0.3
 
-    num_sims = 10
+    num_sims = 100
     # The below is a flag for models that treat the problem as a one stage problem
     simple_modules = ["ucb_over_intervention_pairs", "ts_over_intervention_pairs"]
     # The outputs are stored in the below matrix
-    average_regret_matrix = np.zeros((len(exploration_budgets), len(models)), dtype=np.float32)
-    for budget_index in tqdm(range(len(exploration_budgets)), desc="Budget Progress"):
-        exploration_budget = exploration_budgets[budget_index]
-        print("\nexploration_budget=", exploration_budget)
+    average_regret_matrix = np.zeros((len(num_intermediate_contexts_list), len(models)), dtype=np.float32)
+    for context_index in tqdm(range(len(num_intermediate_contexts_list)), desc="Budget Progress"):
+        num_intermediate_contexts = num_intermediate_contexts_list[context_index]
+        num_causal_variables = num_intermediate_contexts
+        num_interventions = num_causal_variables * 2 + 1
+
+        print("\nnum_intermediate_contexts=", num_intermediate_contexts)
         avg_regret_for_models = run_multiple_sims_multiple_models_stochastic(models, num_sims, exploration_budget,
                                                                              num_intermediate_contexts,
                                                                              num_interventions,
                                                                              diff_prob_transition, default_reward,
                                                                              diff_in_best_reward)
-        average_regret_matrix[budget_index] = avg_regret_for_models
+        average_regret_matrix[context_index] = avg_regret_for_models
 
         # Set print options to display the entire array
         np.set_printoptions(threshold=np.inf)
@@ -111,9 +110,9 @@ if __name__ == "__main__":
         np.set_printoptions(threshold=False)
 
     # Now we saved the obtained values to file.
-    file_path = "outputs/model_regret_with_T.txt"
+    file_path = "outputs/model_regret_with_num_intermediate_contexts.txt"
     # Headers for each column
-    headers = ['exploration_budget'] + models
+    headers = ['num_intermediate_contexts'] + models
     # Open the file for writing
     with open(file_path, 'w') as file:
         # Write the headers as the first line
